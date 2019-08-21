41b6e53ff2f82f722fc4c2c1f75af0df81da0e65
chefe <hi@chefe.io>
2019-08-21 13:09:18 +0200

    Update script to calculate correct offset and size for binary firmware extraction

--- download.sh
+++ download.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+# cleanup files
+rm -rf Firmware_Release/ FW_RT_AC88U_300438445717.zip
 
 # download router firmware
 wget https://dlcdnets.asus.com/pub/ASUS/wireless/RT-AC88U/FW_RT_AC88U_300438445717.zip
@@ -12,8 +15,20 @@ cd Firmware_Release
 # extract firmware file
 7z x RT-AC88U_3.0.0.4_384_45717-gadd52a8.trx
 
+# define variable to binary file for use in the following commands
+file="lib/modules/2.6.36.4brcmarm/kernel/drivers/net/dhd/dhd.ko"
+
+# find the size of the the binary part (driver starts with 4366c0)
+sizeInHex=$(readelf -s $file | grep 4366c0 | grep dlarray | head -n 1 | awk '{ print $3 }')
+
+# convert the size from hex to dec for later use
+sizeInDec=$((16#${sizeInHex:2}))
+
+# find the offset where the binary part starts (00 f2 3e b8 ...)
+offsetInDec=$(od -v -t x1 $file | sed 's/[^ ]* *//' | tr '\012' ' ' | grep -b -i -o "00 f2 3e b8" | sed 's/:.*/\/3/' | bc | head -n 1)
+
 # extract the correct binary part from the firmware
-dd if=lib/modules/2.6.36.4brcmarm/kernel/drivers/net/dhd/dhd.ko skip=1269516 ibs=1 count=935317 of=brcmfmac4366c-pcie.bin
+dd if=$file skip=$offsetInDec ibs=1 count=$sizeInDec of=brcmfmac4366c-pcie.bin
 
 # move firmware a folder up
 mv brcmfmac4366c-pcie.bin ..
@@ -24,6 +39,3 @@ cd ..
 # cleanup files
 rm -r Firmware_Release/ FW_RT_AC88U_300438445717.zip
 
-# echo some hints
-echo "HINT: copy the generated file to /sys/firmware/brcm"
-echo "HINT: run 'iw reg set CH' to set the correct frequencies
